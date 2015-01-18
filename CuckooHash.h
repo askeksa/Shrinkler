ec54c5550117fd84e1892c27a25a707fa690d185
Aske Simon Christensen <askeksa@users.noreply.github.com>
2015-01-18 22:52:49 +0100

    Use bitfields to make cuckoo hash smaller. Do not allocate element array until needed.

--- CuckooHash.h
+++ CuckooHash.h
@@ -18,13 +18,13 @@ template <typename V> class CuckooHash;
 
 template <typename V>
 class CuckooHashIterator {
-	CuckooHash<V>* table;
+	const CuckooHash<V>* table;
 	int index;
-	CuckooHashIterator(CuckooHash<V>* table, int index) : table(table), index(index)
+	CuckooHashIterator(const CuckooHash<V>* table, int index) : table(table), index(index)
 	{}
 
 	void find() {
-		while (table->array[index].first == CuckooHash<V>::UNUSED) index++;
+		while (table->element_array[index].first == CuckooHash<V>::UNUSED) index++;
 	}
 
 	friend class CuckooHash<V>;
@@ -32,12 +32,12 @@ class CuckooHashIterator {
 public:
 	pair<int, V>& operator*() {
 		find();
-		return table->array[index];
+		return table->element_array[index];
 	}
 
 	pair<int, V>* operator->() {
 		find();
-		return &table->array[index];
+		return &table->element_array[index];
 	}
 
 	CuckooHashIterator<V> operator++(int) {
@@ -64,29 +64,36 @@ private:
 	static const key_type UNUSED = 0x80000000;
 	static const hash_type HASH1_MUL = 0xF230D3A1;
 	static const hash_type HASH2_MUL = 0x8084027F;
-	static const int INITIAL_SIZE_LOG = 4;
+	static const int INITIAL_SIZE_LOG = 2;
 
-	value_type* array;
-	int n_elements;
-	int hash_shift;
+	value_type* element_array;
+	unsigned n_elements:26;
+	unsigned hash_shift:6;
 
-	int array_size() {
+	int array_size() const {
 		return 1 << (sizeof(hash_type) * 8 - hash_shift);
 	}
 
 	void init_array() {
 		int size = array_size();
-		array = new value_type[size];
+		element_array = new value_type[size];
 		for (int i = 0 ; i < size ; i++) {
-			array[i].first = UNUSED;
-			array[i].second = V();
+			element_array[i].first = UNUSED;
+			element_array[i].second = V();
 		}
+	}
+
+	value_type* get_array() {
+		if (element_array == NULL) {
+			init_array();
+		}
+		return element_array;
 	}
 
 	void init() {
 		n_elements = 0;
 		hash_shift = sizeof(hash_type) * 8 - INITIAL_SIZE_LOG;
-		init_array();
+		element_array = NULL;
 	}
 
 	void hashes(key_type key, hash_type& hash1, hash_type& hash2) const {
@@ -97,7 +104,8 @@ private:
 
 	void rehash() {
 		int old_size = array_size();
-		value_type* old_array = array;
+		value_type* old_array = get_array();
+		n_elements = 0;
 		hash_shift--;
 		init_array();
 		for (int i = 0 ; i < old_size ; i++) {
@@ -109,6 +117,7 @@ private:
 	}
 
 	void insert(hash_type hash, int key, V value, int n) {
+		value_type* array = get_array();
 		while (array[hash].first != UNUSED) {
 			if (--n < 0) {
 				rehash();
@@ -138,20 +147,27 @@ public:
 	}
 
 	~CuckooHash() {
-		delete[] array;
+		delete[] element_array;
 	}
 
 	void clear() {
-		delete[] array;
+		delete[] element_array;
 		init();
 	}
 
-	iterator begin() {
+	iterator begin() const {
 		return CuckooHashIterator<V>(this, 0);
 	}
 
-	iterator end() {
+	iterator end() const {
+		if (element_array == NULL) {
+			// Empty
+			return CuckooHashIterator<V>(this, 0);
+		}
+
 		int index = array_size();
+		assert(element_array != NULL);
+		value_type* array = element_array;
 		while (index > 0 && array[index - 1].first == UNUSED) index--;
 		return CuckooHashIterator<V>(this, index);
 	}
@@ -165,10 +181,14 @@ public:
 	}
 
 	int count(int key) const {
+		if (empty()) return 0;
+
 		hash_type hash1;
 		hash_type hash2;
 		hashes(key, hash1, hash2);
 
+		assert(element_array != NULL);
+		value_type* array = element_array;
 		if (array[hash1].first == key || array[hash2].first == key) return 1;
 		return 0;
 	}
@@ -178,6 +198,7 @@ public:
 		hash_type hash2;
 		hashes(key, hash1, hash2);
 
+		value_type* array = get_array();
 		hash_type hash;
 		if (array[hash1].first == key) {
 			hash = hash1;
@@ -196,6 +217,7 @@ public:
 		hash_type hash2;
 		hashes(key, hash1, hash2);
 
+		value_type* array = get_array();
 		if (array[hash1].first == key) return array[hash1].second;
 		if (array[hash2].first == key) return array[hash2].second;
 		if (array[hash1].first == UNUSED) {
@@ -210,7 +232,7 @@ public:
 			n_elements++;
 			return array[hash2].second;
 		}
-		insert(hash1, key, V(), 42);
+		insert(hash1, key, V(), n_elements);
 		return (*this)[key];
 	}
 };
